555778358b90e37ab3d993ebfefe5bf8ae071b7d
Paul Nielsen <pacanielsen@gmail.com>
2018-11-29 19:30:07 -0700

    print company to console

--- public/transactions.js
+++ public/transactions.js
@@ -1,4 +1,19 @@
 // JavaScript source code
 function add() {
-    document.getElementById("addText").innerHTML = "It worked!";
+ 
+    let company = $("#company").val();
+    console.log("Company: " + company);
+
+    $.get("/transactions", function(data) {
+        console.log(data);
+
+        for (var i = 0; i < data.company.length; i++) {
+            var company = data.company[i];
+            $("body").append(company);
+            //var date = data.date[i];
+            //$("body").append(date);
+            //var amount = data.amount[i];
+            //$("body").append(amount);
+        }
+    })
 }
